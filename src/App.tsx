fe696fd6e2776c3831dd9524fa5c33d499322794
yena0213 <yeana3131@naver.com>
2024-08-24 16:25:50 +0900

    [set]기초세팅

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import axios from 'axios';
+import { z } from 'zod';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* 추가적인 라우트 설정 */}
+        </Routes>
+      </Router>
+    </QueryClientProvider>
+  );
+}
+
+function Home() {
+  return <div>Home Page</div>;
+}
+
+export default App;
