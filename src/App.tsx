90f88e23e71229aac41536100ce5f7581ad6c960
yena0213 <yeana3131@naver.com>
2024-08-28 18:01:24 +0900

    [Design] Navbar 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import axios from 'axios';
-import { z } from 'zod';
+import Navbar from './componenets/Navbar';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* 추가적인 라우트 설정 */}
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+    <Navbar/>
   );
 }
 
-function Home() {
-  return <div>Home Page</div>;
-}
 
 export default App;
