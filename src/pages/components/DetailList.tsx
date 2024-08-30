59b648e5a6e422ff4d91f8cdcb5baad9d6d88707
Sohyunnnn <thgus05061@naver.com>
2024-08-30 21:20:25 +0900

    #13 [Feat] 현재 날짜 가져오도록 기능 구현

--- src/pages/components/DetailList.tsx
+++ src/pages/components/DetailList.tsx
@@ -26,6 +26,18 @@ const DetailList: React.FC = () => {
   const endIndex = startIndex + ITEMS_PER_PAGE;
   const currentData = data.slice(startIndex, endIndex);
 
+  const getCurrentDateWithDay = () => {
+    const today = new Date();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+    const dayOfWeek = daysOfWeek[today.getDay()];
+
+    return { date: `${month}월 ${day}일`, dayOfWeek: `${dayOfWeek}` };
+  };
+
+  const { date, dayOfWeek } = getCurrentDateWithDay();
+
   return (
     <Box
       sx={{
@@ -34,11 +46,17 @@ const DetailList: React.FC = () => {
         padding: 2,
       }}
     >
-        <Box>
-          <Typography>
-          세부 일정
+      <Box display="flex" alignItems="center" mb={2}>
+        <Typography variant="h6">세부 일정</Typography>
+        <Box ml={2}>
+          <Typography variant="body2" component="span" sx={{ fontWeight: 'bold' }}>
+            {date}
+          </Typography>
+          <Typography variant="body2" component="span" sx={{ color: 'gray' }}>
+            {dayOfWeek}
           </Typography>
         </Box>
+      </Box>
       
       <Paper elevation={3} sx={{ padding: 2 }}>
         {currentData.map(item => (
@@ -47,9 +65,11 @@ const DetailList: React.FC = () => {
             sx={{
               border: '1px solid #ddd',
               borderRadius: 1,
+              mb: 2, 
+              p: 2,
             }}
           >
-            <Typography >{item.title}</Typography>
+            <Typography variant="h6">{item.title}</Typography>
             <Typography>{item.description}</Typography>
           </Box>
         ))}
