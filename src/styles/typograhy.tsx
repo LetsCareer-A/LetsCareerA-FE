ff07717867292272d0edba04e9620262b6937ff4
Sohyunnnn <thgus05061@naver.com>
2024-08-31 00:56:22 +0900

    #14 [Design] 텍스트 디자인 시스템 정의

---
+++ src/styles/typograhy.tsx
@@ -0,0 +1,365 @@
+const typography = {
+  //xxlarge
+  xxlarge1: {
+    fontFamily: 'Pretendard',
+    fontSize: '36px',
+    fontWeight: 600,
+    lineHeight: '48px',
+    letterSpacing: '-1.008px',
+    fontStyle: 'normal',
+  },
+  xxlarge2: {
+    fontFamily: 'Pretendard',
+    fontSize: '32px',
+    fontWeight: 500,
+    lineHeight: '42px',
+    letterSpacing: '-0.832px',
+    fontStyle: 'normal',
+  },
+  //xlarge
+  xlargeBold: {
+    fontFamily: 'Pretendard',
+    fontSize: '28px',
+    fontWeight: 700,
+    lineHeight: '38px',
+    letterSpacing: '-0.7px',
+    fontStyle: 'normal',
+  },
+  xlargeSemibold: {
+    fontFamily: 'Pretendard',
+    fontSize: '28px',
+    fontWeight: 600,
+    lineHeight: '38px',
+    letterSpacing: '-0.7px',
+    fontStyle: 'normal',
+  },
+  xlargeMed: {
+    fontFamily: 'Pretendard',
+    fontSize: '28px',
+    fontWeight: 500,
+    lineHeight: '38px',
+    letterSpacing: '-0.7px',
+    fontStyle: 'normal',
+  },
+  //large
+  largeSemibold: {
+    fontFamily: 'Pretendard',
+    fontSize: '26px',
+    fontWeight: 600,
+    lineHeight: '34px',
+    letterSpacing: '-0.624px',
+    fontStyle: 'normal',
+  },
+  largeMed: {
+    fontFamily: 'Pretendard',
+    fontSize: '26px',
+    fontWeight: 500,
+    lineHeight: '34px',
+    letterSpacing: '-0.624px',
+    fontStyle: 'normal',
+  },
+  largeLight: {
+    fontFamily: 'Pretendard',
+    fontSize: '26px',
+    fontWeight: 300,
+    lineHeight: '34px',
+    letterSpacing: '-0.624px',
+    fontStyle: 'normal',
+  },
+  //medium
+
+  mediumBold: {
+    fontFamily: 'Pretendard',
+    fontSize: '24px',
+    fontWeight: 700,
+    lineHeight: '32px',
+    letterSpacing: '-0.576px',
+    fontStyle: 'normal',
+  },
+
+  mediumSemibold: {
+    fontFamily: 'Pretendard',
+    fontSize: '24px',
+    fontWeight: 600,
+    lineHeight: '32px',
+    letterSpacing: '-0.576px',
+    fontStyle: 'normal',
+  },
+
+  mediumMed: {
+    fontFamily: 'Pretendard',
+    fontSize: '24px',
+    fontWeight: 500,
+    lineHeight: '32px',
+    letterSpacing: '-0.576px',
+    fontStyle: 'normal',
+  },
+
+  mediumLight: {
+    fontFamily: 'Pretendard',
+    fontSize: '24px',
+    fontWeight: 300,
+    lineHeight: '32px',
+    letterSpacing: '-0.576px',
+    fontStyle: 'normal',
+  },
+
+  medium2Bold: {
+    fontFamily: 'Pretendard',
+    fontSize: '22px',
+    fontWeight: 700,
+    lineHeight: '30px',
+    letterSpacing: '-0.576px',
+    fontStyle: 'normal',
+  },
+
+  medium2Semibold: {
+    fontFamily: 'Pretendard',
+    fontSize: '22px',
+    fontWeight: 600,
+    lineHeight: '30px',
+    letterSpacing: '-0.576px',
+    fontStyle: 'normal',
+  },
+
+  medium2Med: {
+    fontFamily: 'Pretendard',
+    fontSize: '22px',
+    fontWeight: 500,
+    lineHeight: '30px',
+    letterSpacing: '-0.576px',
+    fontStyle: 'normal',
+  },
+  medium2Light: {
+    fontFamily: 'Pretendard',
+    fontSize: '22px',
+    fontWeight: 300,
+    lineHeight: '30px',
+    letterSpacing: '-0.576px',
+    fontStyle: 'normal',
+  },
+  medium2Thin: {
+    fontFamily: 'Pretendard',
+    fontSize: '22px',
+    fontWeight: 200,
+    lineHeight: '30px',
+    letterSpacing: '-0.576px',
+    fontStyle: 'normal',
+  },
+
+  //small
+  samllBold: {
+    fontFamily: 'Pretendard',
+    fontSize: '20px',
+    fontWeight: 700,
+    lineHeight: '28px',
+    fontStyle: 'normal',
+  },
+  samllSemibold: {
+    fontFamily: 'Pretendard',
+    fontSize: '20px',
+    fontWeight: 600,
+    lineHeight: '28px',
+    fontStyle: 'normal',
+  },
+  samllMed: {
+    fontFamily: 'Pretendard',
+    fontSize: '20px',
+    fontWeight: 500,
+    lineHeight: '28px',
+    letterSpacing: '-0.4px',
+    fontStyle: 'normal',
+  },
+  samllReg: {
+    fontFamily: 'Pretendard',
+    fontSize: '20px',
+    fontWeight: 400,
+    lineHeight: '28px',
+    letterSpacing: '-0.4px',
+    fontStyle: 'normal',
+  },
+  samllLight: {
+    fontFamily: 'Pretendard',
+    fontSize: '20px',
+    fontWeight: 300,
+    lineHeight: '28px',
+    letterSpacing: '-0.4px',
+    fontStyle: 'normal',
+  },
+
+
+  samll2Bold: {
+    fontFamily: 'Pretendard',
+    fontSize: '18px',
+    fontWeight: 700,
+    lineHeight: '26px',
+    fontStyle: 'normal',
+  },
+  samll2Semibold: {
+    fontFamily: 'Pretendard',
+    fontSize: '18px',
+    fontWeight: 600,
+    lineHeight: '26px',
+    fontStyle: 'normal',
+    letterSpacing: '-0.022px',
+  },
+  samll2Med: {
+    fontFamily: 'Pretendard',
+    fontSize: '18px',
+    fontWeight: 500,
+    lineHeight: '26px',
+    letterSpacing: '-0.022pxpx',
+    fontStyle: 'normal',
+  },
+  samll2Reg: {
+    fontFamily: 'Pretendard',
+    fontSize: '18px',
+    fontWeight: 400,
+    lineHeight: '26px',
+    letterSpacing: '-0.022px',
+    fontStyle: 'normal',
+  },
+  samll2Light: {
+    fontFamily: 'Pretendard',
+    fontSize: '18px',
+    fontWeight: 300,
+    lineHeight: '26px',
+    letterSpacing: '-0.022px',
+    fontStyle: 'normal',
+  },
+
+  //xsmall
+  xSmallBold: {
+    fontFamily: 'Pretendard',
+    fontSize: '16px',
+    fontWeight: 700,
+    lineHeight: '24px',
+    letterSpacing: '-0.096px',
+    fontStyle: 'normal',
+  },
+  xSmallSemiold: {
+    fontFamily: 'Pretendard',
+    fontSize: '16px',
+    fontWeight: 600,
+    lineHeight: '24px',
+    letterSpacing: '-0.096px',
+    fontStyle: 'normal',
+  },
+  xSmallMed: {
+    fontFamily: 'Pretendard',
+    fontSize: '16px',
+    fontWeight: 500,
+    lineHeight: '24px',
+    letterSpacing: '-0.096px',
+    fontStyle: 'normal',
+  },
+  xSmallLight: {
+    fontFamily: 'Pretendard',
+    fontSize: '16px',
+    fontWeight: 300,
+    lineHeight: '24px',
+    letterSpacing: '-0.096px',
+    fontStyle: 'normal',
+  },
+
+  xSmall2Bold: {
+    fontFamily: 'Pretendard',
+    fontSize: '14px',
+    fontWeight: 700,
+    lineHeight: '20px',
+    letterSpacing: '-0.21px',
+    fontStyle: 'normal',
+  },
+  xSmall2Bold: {
+    fontFamily: 'Pretendard',
+    fontSize: '14px',
+    fontWeight: 700,
+    lineHeight: '20px',
+    letterSpacing: '-0.21px',
+    fontStyle: 'normal',
+  },
+  xSmall2Semiold: {
+    fontFamily: 'Pretendard',
+    fontSize: '14px',
+    fontWeight: 600,
+    lineHeight: '20px',
+    letterSpacing: '-0.21px',
+    fontStyle: 'normal',
+  },
+  xSmall2Med: {
+    fontFamily: 'Pretendard',
+    fontSize: '14px',
+    fontWeight: 500,
+    lineHeight: '20px',
+    letterSpacing: '-0.21px',
+    fontStyle: 'normal',
+  },
+  xSmall2Reg: {
+    fontFamily: 'Pretendard',
+    fontSize: '14px',
+    fontWeight: 400,
+    lineHeight: '20px',
+    letterSpacing: '-0.21px',
+    fontStyle: 'normal',
+  },
+  xSmall2Light: {
+    fontFamily: 'Pretendard',
+    fontSize: '14px',
+    fontWeight: 300,
+    lineHeight: '20px',
+    letterSpacing: '-0.21px',
+    fontStyle: 'normal',
+  },
+
+  //xxSmall
+  xxSmallBold: {
+    fontFamily: 'Pretendard',
+    fontSize: '12px',
+    fontWeight: 700,
+    lineHeight: '16px',
+    letterSpacing: '-0.3px',
+    fontStyle: 'normal',
+  },
+  xxSmallSemibold: {
+    fontFamily: 'Pretendard',
+    fontSize: '12px',
+    fontWeight: 500,
+    lineHeight: '16px',
+    letterSpacing: '-0.3px',
+    fontStyle: 'normal',
+  },
+  xxSmallMed: {
+    fontFamily: 'Pretendard',
+    fontSize: '12px',
+    fontWeight: 500,
+    lineHeight: '16px',
+    letterSpacing: '-0.3px',
+    fontStyle: 'normal',
+  },
+  xxSmallReg: {
+    fontFamily: 'Pretendard',
+    fontSize: '12px',
+    fontWeight: 400,
+    lineHeight: '16px',
+    letterSpacing: '-0.3px',
+    fontStyle: 'normal',
+  },
+  xxSmall2: {
+    fontFamily: 'Pretendard',
+    fontSize: '11px',
+    fontWeight: 400,
+    lineHeight: '150%',
+    letterSpacing: '-0.363px',
+    fontStyle: 'normal',
+  },
+
+
+
+
+
+
+
+
+};
+
+export default typography;
