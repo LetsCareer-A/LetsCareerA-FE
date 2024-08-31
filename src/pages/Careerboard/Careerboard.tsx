e59d424085dd632fc6c6b10bd41a38889d5dbd68
Sohyunnnn <thgus05061@naver.com>
2024-08-31 19:12:07 +0900

    #19 [Chore] 프로젝트 구조 설정

---
+++ src/pages/Careerboard/Careerboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Careerboard = () => {
+  return (
+    <div>Careerboard</div>
+  )
+}
+
+export default Careerboard
---
+++ src/pages/Retrospective/Retrospective.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Retrospective = () => {
+  return (
+    <div>Retrospective</div>
+  )
+}
+
+export default Retrospective
