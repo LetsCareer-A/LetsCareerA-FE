d8aea196d641eb7b06237ff35824995fef2397a0
Sohyunnnn <thgus05061@naver.com>
2024-09-05 01:20:38 +0900

    #30 [Feat] 경험관리 모달 상태관리 추가

---
+++ src/store/careerModalStore.tsx
@@ -0,0 +1,31 @@
+import create from 'zustand';
+
+interface CareerModalStore {
+  isModalOpen: boolean;
+  setIsModalOpen: (open: boolean) => void;
+  title: string;
+  setTitle: (title: string) => void;
+  situation: string;
+  setSituation: (situation: string) => void;
+  task: string;
+  setTask: (task: string) => void;
+  action: string;
+  setAction: (action: string) => void;
+  result: string;
+  setResult: (result: string) => void;
+}
+
+export const useStore = create<CareerModalStore>((set) => ({
+  isModalOpen: false,
+  setIsModalOpen: (open) => set({ isModalOpen: open }),
+  title: '',
+  setTitle: (title) => set({ title }),
+  situation: '',
+  setSituation: (situation) => set({ situation }),
+  task: '',
+  setTask: (task) => set({ task }),
+  action: '',
+  setAction: (action) => set({ action }),
+  result: '',
+  setResult: (result) => set({ result }),
+}));
