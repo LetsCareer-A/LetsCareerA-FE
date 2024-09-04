4b28f2928f24560a76e2945bddd72ab12fcf3f4c
yena0213 <yeana3131@naver.com>
2024-09-05 07:17:43 +0900

    #32 [Design] 화면 구성 틀잡기'

---
+++ src/pages/StepDetail/StepDetailPage.tsx
@@ -0,0 +1,21 @@
+import { Box, Stack } from '@mui/material';
+import React from 'react';
+
+const StepDetailPage = () => {
+    return (
+        <Box sx={{display: 'flex', left: '40px', top: '40px'}}>
+            {/* title */}
+            <Stack spacing={16}>
+
+            </Stack>
+           {/* 서류 전형 단계 박스 */}
+           {/* 배너 */}
+           {/* 컨텐츠 */}
+                {/* 자기소개서 */}
+                {/* 핵심경험 */}
+
+        </Box>
+    );
+};
+
+export default StepDetailPage;
