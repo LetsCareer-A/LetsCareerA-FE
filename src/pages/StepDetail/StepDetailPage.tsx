b6fab53d02e5418674baddcf8c654cd3ef3bf4e5
Sohyunnnn <thgus05061@naver.com>
2024-09-05 23:02:42 +0900

    [Fix] 빌드 오류 수정

--- src/pages/StepDetail/StepDetailPage.tsx
+++ src/pages/StepDetail/StepDetailPage.tsx
@@ -1,8 +1,6 @@
 import { Box, Stack, Typography,  } from '@mui/material';
 import typograpy from '../../styles/typography';
 import colors from '../../styles/colors';
-import document from 'public/images/document.svg';
-import banner from 'public/images/banner.png';
 import { purple } from '@mui/material/colors';
 
 
@@ -13,9 +11,9 @@ const StepDetailPage = () => {
             <Stack spacing={16}>
                 {/* backicon */}
                 {/* chip */}
-                <Typography color={colors.neutral[10]} typography={typograpy.mediumBold}>삼성전자</Typography>
-                <Typography color={colors.neutral[10]} typography={typograpy.mediumBold}>|</Typography>
-                <Typography color={colors.neutral[10]} typography={typograpy.mediumBold}>직무</Typography>
+                <Typography color={colors.neutral[10]} style={typograpy.mediumBold}>삼성전자</Typography>
+                <Typography color={colors.neutral[10]} style={typograpy.mediumBold}>|</Typography>
+                <Typography color={colors.neutral[10]} style={typograpy.mediumBold}>직무</Typography>
                 {/* input togle */}
             </Stack>
 
@@ -23,8 +21,8 @@ const StepDetailPage = () => {
                 {/* 서류 전형 단계 박스 */}
                 <Box sx={{display: 'flex', width: '100%', flexDirection: 'column', padding: '25px 25px', borderRadius: '12px', border: `1px solid ${colors.neutral[85]}`, bgcolor: `${colors.neutral[100]}`}}>
                     <Stack spacing={'4px'} direction={'column'}><Box sx={{width: 95, height:95, marginLeft: '25px', marginTop:'31px', bgcolor: `${purple}`}}></Box>
-                    <Typography color={colors.neutral[10]} typography={typograpy.xSmallMed}>서류전형</Typography>
-                    <Typography color={colors.neutral[40]} typography={typograpy.xxSmallReg}>24.08.30</Typography>
+                    <Typography color={colors.neutral[10]} style={typograpy.xSmallMed}>서류전형</Typography>
+                    <Typography color={colors.neutral[40]} style={typograpy.xxSmallReg}>24.08.30</Typography>
                     </Stack>
                     {/* chip */}
                 </Box>
@@ -37,12 +35,12 @@ const StepDetailPage = () => {
                     {/* 자기소개서 */}
                     <Box sx={{display: 'inline-flex', padding: '16px', alignItems: 'center', gap: '10px', border: `1px solid ${colors.neutral[85]}`, bgcolor: `${colors.neutral[100]}`}}>
                         <Box gap={'16px'}>
-                            <Typography color={colors.neutral[10]} typography={typograpy.smallBold}>자기소개서</Typography>
-                            <Typography color={colors.neutral[45]} typography={typograpy.xSmall2Med}>준비하는 기업의 자기소개서를 미리 써봐요.</Typography>
+                            <Typography color={colors.neutral[10]} style={typograpy.smallBold}>자기소개서</Typography>
+                            <Typography color={colors.neutral[45]} style={typograpy.xSmall2Med}>준비하는 기업의 자기소개서를 미리 써봐요.</Typography>
                             {/* 문항추가하기 버튼 추가 */}
                         </Box>
                         <Box gap={'8px'} display={'flex'} flexDirection={'column'}>
-                            <Typography color={colors.neutral[10]} typography={typograpy.xSmallBold}>문항1</Typography>
+                            <Typography color={colors.neutral[10]} style={typograpy.xSmallBold}>문항1</Typography>
                             {/* 텍스트필드 추가 */}
                         </Box>
                         {/* 텍스트필드 추가 */}
@@ -50,8 +48,8 @@ const StepDetailPage = () => {
                     {/* 핵심경험 */}
                     <Box sx={{display: 'inline-flex', padding: '16px 15px', flexDirection: 'column', justifyContent: 'center', alignItems:'flex-start', gap: '15px', borderRadius:'12px', border: `1px solid ${colors.neutral[85]}`, bgcolor: `${colors.neutral[100]}`}}>
                         <Box gap={'8px'}>
-                            <Typography color={colors.neutral[10]} typography={typograpy.smallBold}>핵심경험</Typography>
-                            <Typography color={colors.neutral[45]} typography={typograpy.xSmall2Med}>준비하는 기업의 자기소개서를 미리 써봐요.</Typography>
+                            <Typography color={colors.neutral[10]} style={typograpy.smallBold}>핵심경험</Typography>
+                            <Typography color={colors.neutral[45]} style={typograpy.xSmall2Med}>준비하는 기업의 자기소개서를 미리 써봐요.</Typography>
                         </Box>
                         <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: '8px'}}>
                             {/* card box 추가 */}
