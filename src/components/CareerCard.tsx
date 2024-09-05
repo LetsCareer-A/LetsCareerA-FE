e11d2ac15d84027ea880fa59f0978adbcbd42d84
Sohyunnnn <thgus05061@naver.com>
2024-09-06 01:41:48 +0900

    #38 [Design] 커리어 카드 체크박스 추가

--- src/components/CareerCard.tsx
+++ src/components/CareerCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Checkbox } from "@mui/material";
 import Chip from "./Chips";
 import colors from "../styles/colors";
 import typography from "../styles/typography";
@@ -13,10 +13,23 @@ const CareerCard = () => {
         border: `1px solid ${colors.neutral[85]}`, 
         background: colors.neutral[95], 
         boxShadow: '0px 0px 29px 0px rgba(0, 0, 0, 0.04)', 
+        position: 'relative' 
       }}
     >
-      <Box mb='8px'>
+      <Box 
+        display='flex'
+        justifyContent='space-between'
+        alignItems='center'
+        mb='8px'
+      >
         <Chip text='dd' />
+        <Checkbox
+          sx={{
+            position: 'absolute',
+            top: '12px',
+            right: '12px'
+          }}
+        />
       </Box>
       <Box>
         <Typography style={typography.small2Bold}>dkdkdk</Typography>
