65d7e1bae3a815cabe93b21841c988b54ad65d53
Sohyunnnn <thgus05061@naver.com>
2024-09-07 13:30:39 +0900

    [Fix] 충돌 수정

--- src/components/CareerMenu.tsx
+++ src/components/CareerMenu.tsx
@@ -98,6 +98,25 @@ const CareerMenu = () => {
           어필할 경험 추가 완료하기
         </PrimaryButton>
       </Box>
+
+      <Box
+        sx={{
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          width: '381px',
+          height: '132px',
+          padding: '20px 40px 68px 40px',
+          backgroundColor: '#FFF',
+          boxShadow: '0px 16px 20px rgba(0, 0, 0, 0.12), 0px 8px 16px rgba(0, 0, 0, 0.08), 0px 0px 8px rgba(0, 0, 0, 0.08)',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexShrink: 0
+        }}
+      >
+        <PrimaryButton>어필할 커리어 추가 완료하기</PrimaryButton>
+      </Box>
     </Box>
   );
 };
