fd588975ab2e0775bc0a6898732a27d19dede182
Sohyunnnn <thgus05061@naver.com>
2024-09-08 02:30:25 +0900

    #47 [Feat] 카테고리별로 배경색 및 텍스트 색 다르게 보이도록 기능 구현

--- src/pages/Careers/components/Card.tsx
+++ src/pages/Careers/components/Card.tsx
@@ -12,43 +12,87 @@ interface CardProps {
   onClick?: () => void;
 }
 
-const Card: React.FC<CardProps> = ({ chipText, chipBackgroundColor, chipTextColor, title, summary, onClick }) => (
-  <Box 
-    sx={{ 
-      width: '337px', 
-      height:'104px',
-      background: 'white', 
-      borderRadius: '12px', 
-      padding: '12px',
-      cursor: 'pointer', 
-      '&:hover': {
-        boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)'
-      }
-    }}
-    onClick={onClick}
-  >
-    <Chip 
-      text={chipText} 
-      backgroundColor={chipBackgroundColor} 
-      textColor={chipTextColor}
-    />
-    <Typography style={typography.small2Bold} color={colors.neutral[10]} mt="8px">
-      {title}
-    </Typography>
-    <Typography 
-      style={typography.xSmall2Reg}
-      color={colors.neutral[60]} 
-      sx={{
-        display: '-webkit-box',
-        WebkitBoxOrient: 'vertical',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
-        WebkitLineClamp: 1
+const Card: React.FC<CardProps> = ({ chipText, chipBackgroundColor, chipTextColor, title, summary, onClick }) => {
+  const getChipColors = (text: string) => {
+    switch (text) {
+      case '대외활동':
+        return {
+          backgroundColor: 'rgba(81, 119, 255, 0.10)',
+          textColor: '#5177FF'
+        };
+      case '실무':
+        return {
+          backgroundColor: ' rgba(255, 86, 106, 0.10)',
+          textColor: '#FF566A'
+        };
+      case '공모전':
+        return {
+          backgroundColor: colors.secondary[10],
+          textColor: colors.secondary.normal,
+        };
+      case '프로젝트':
+        return {
+          backgroundColor: colors.primary[10],
+          textColor: colors.primary.normal
+        };
+      case '자격증':
+        return {
+          backgroundColor: 'rgba(255, 143, 81, 0.15)',
+          textColor: '#FF8F51'
+        };
+      case '기타':
+        return {
+          backgroundColor: colors.neutral[90],
+          textColor:  colors.neutral[20],
+        };
+      default:
+        return {
+          backgroundColor: chipBackgroundColor,
+          textColor: chipTextColor
+        };
+    }
+  };
+
+  const { backgroundColor, textColor } = getChipColors(chipText);
+
+  return (
+    <Box 
+      sx={{ 
+        width: '337px', 
+        height:'104px',
+        background: 'white', 
+        borderRadius: '12px', 
+        padding: '12px',
+        cursor: 'pointer', 
+        '&:hover': {
+          boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)'
+        }
       }}
+      onClick={onClick}
     >
-      {summary}
-    </Typography>
-  </Box>
-);
+      <Chip 
+        text={chipText} 
+        backgroundColor={backgroundColor} 
+        textColor={textColor}
+      />
+      <Typography style={typography.small2Bold} color={colors.neutral[10]} mt="8px">
+        {title}
+      </Typography>
+      <Typography 
+        style={typography.xSmall2Reg}
+        color={colors.neutral[60]} 
+        sx={{
+          display: '-webkit-box',
+          WebkitBoxOrient: 'vertical',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          WebkitLineClamp: 1
+        }}
+      >
+        {summary}
+      </Typography>
+    </Box>
+  );
+};
 
 export default Card;
