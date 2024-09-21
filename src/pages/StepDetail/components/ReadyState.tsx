2fd4d957e6017133b98bf18f758baf64a018cf5d
yena0213 <yeana3131@naver.com>
2024-09-22 02:57:07 +0900

    #90 [Feat] 불합격 드롭다운 속성 변경

--- src/pages/StepDetail/components/ReadyState.tsx
+++ src/pages/StepDetail/components/ReadyState.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, FormControl, MenuItem, Select, Typography } from '@mui/material';
 import Dropdown, { DropdownItem } from '../../../components/Dropdown';
 import colors from '../../../styles/colors';
 import typography from '../../../styles/typography';
@@ -9,14 +9,16 @@ import AddStateModal from './AddStateModal';
 
 import useScheduleStore from '../../../store/useScheduleStore';
 
-const Stateitems: DropdownItem[] = [
-  { text: '진행중', color: `${colors.primary[10]}`, textColor: `${colors.primary.normal}` },
-  { text: '진행완료', color: `${colors.primary[10]}`, textColor: `${colors.primary.normal}` },
-  { text: '합격', color: `${colors.primary[10]}`, textColor: `${colors.primary.normal}` },
-  { text: '불합격', color: `${colors.primary[10]}`, textColor: `${colors.primary.normal}` },
+const Stateitems = [
+  { text: '진행중', color: colors.primary[10], textColor: colors.primary.normal },
+  { text: '진행완료', color: colors.primary[10], textColor: colors.primary.normal },
+  { text: '합격', color: colors.primary[10], textColor: colors.primary.normal },
+  { text: '불합격', color: 'rgba(255, 86, 106, 0.10)', textColor: '#FF566A' },
 ];
 
 const StateGroup = ({ stage }: { stage: Stage }) => {
+  const [selectedState, setSelectedState] = useState('');
+
   const setSelectedStageId = useScheduleStore((state) => state.setSelectedStageId);
   const setSelectedStageType = useScheduleStore((state) => state.setSelectedStageType);
 
@@ -24,6 +26,11 @@ const StateGroup = ({ stage }: { stage: Stage }) => {
     setSelectedStageId(stage.stageId);
     setSelectedStageType(stage.type);
   };
+
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setSelectedState(event.target.value);
+  };
+  
   return(
   <Box
     sx={{
@@ -62,17 +69,44 @@ const StateGroup = ({ stage }: { stage: Stage }) => {
       {stage.date || '날짜'}
     </Typography>
 
-    <Dropdown
-      buttonText="상태"
-      backgroundColor={colors.primary[10]}
-      items={Stateitems}
-      sx={{
-        height: '28px',
-        marginTop: '10px',
-        color: `${colors.primary.normal}`,
-        typography: `${typography.xSmall2Med}`,
-      }}
-    />
+    <FormControl sx={{ width: '100%', marginTop: '10px' }}>
+      <Select
+        value={selectedState || '진행중'} // selectedState가 없으면 '진행중'으로 설정
+        onChange={handleChange}
+        sx={{
+          height: '28px',
+          typography: typography.xSmall2Reg,
+          textAlign: 'center', 
+          background: selectedState == '불합격'? 'rgba(255, 86, 106, 0.10)': `${colors.primary[10]}`,
+          color: selectedState == '불합격'? '#FF566A': `${colors.primary.normal }`,
+          '& .MuiOutlinedInput-notchedOutline': {
+            border: 'none',
+          },
+        }}
+      >
+        {Stateitems.map((item) => (
+          <MenuItem
+            key={item.text}
+            value={item.text}
+            sx={{
+              display: 'flex',
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'white',
+              color: 'black',
+              typography: typography.xSmall2Reg,
+              '&:hover': {
+                backgroundColor: `${item.color}80`, // 호버 시 배경색 약간 투명하게
+                color: item.textColor,
+              },
+            }}
+          >
+            {item.text}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
   </Box>
   )
 };
